8c6bd33b05c5eb40b3fff72ba65759174cfc8255
shannonh800 <sh5572@stern.nyu.edu>
2022-11-29 10:09:30 -0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>Login</h2>
+  <div class="error" v-if="errorMessage">{{ message }}</div>
+  <LoginForm msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  },
+  data() {
+    return {
+      errorMessage: "Test Message" // how to get error message that's located on the backend?? & where do i post to?
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
